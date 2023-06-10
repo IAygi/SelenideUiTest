bb3bd0201080ce5e562f71f6b7ab2152740f1ada
Krack Digger <grave1kro@gmail.com>
2023-06-10 12:14:36 +0300

    edit Jenkinsfile

--- src/test/java/ru/krackdigger/MainTest.java
+++ src/test/java/ru/krackdigger/MainTest.java
@@ -2,6 +2,7 @@ package ru.krackdigger;
 
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -21,14 +22,16 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class MainTest {
 
+    RemoteWebDriver driver;
+
     @BeforeEach
     public void initDriver() throws IOException {
         ChromeOptions options = new ChromeOptions();
         options.setCapability("selenoid:options", new HashMap<String, Object>() {{
             put("name", "My test");
-            put("sessionTimeout", "1m");
+            put("sessionTimeout", "5m");
             put("screenResolution", "1920x1080x24");
-//            put("enableVNC", true);
+            put("enableVNC", true);
             put("env", new ArrayList<String>() {{
                 add("TZ=UTC");
             }});
@@ -37,22 +40,23 @@ public class MainTest {
             }});
             put("enableVideo", true);
         }});
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://194.67.119.85:4444/wd/hub"), options);
+        driver = new RemoteWebDriver(new URL("http://194.67.119.85:4444/wd/hub"), options);
         driver.manage().window().setSize(new Dimension(1920,1080));
         WebDriverRunner.setWebDriver(driver);
     }
 
     @AfterEach
     public void stopDriver() {
-        Optional.of(WebDriverRunner.getWebDriver()).ifPresent(WebDriver::quit);
+        driver.quit();
     }
 
     @Test
-    public void userCanLoginByUsername() {
+    public void simpleTest() {
         open("https://google.com");
         $(By.name("q")).setValue("text").pressEnter();
 //        $("#submit").click();
 //        $(".loading_progress").should(disappear); // Само подождёт, пока элемент исчезнет
 //        $("#username").shouldHave(text("Hello, Johny!")); // Само подождёт, пока у элемента появится нужный текст
+        Assertions.assertTrue(true);
     }
 }
