a71b5163d904cd4a159006b3d2317bcb0ebeb803
Krack Digger <grave1kro@gmail.com>
2023-06-11 15:03:09 +0300

    edit Jenkinsfile

--- src/test/java/ru/krackdigger/MainTest.java
+++ src/test/java/ru/krackdigger/MainTest.java
@@ -1,13 +1,11 @@
 package ru.krackdigger;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -15,11 +13,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Optional;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 
@@ -30,9 +27,9 @@ public class MainTest {
         ChromeOptions options = new ChromeOptions();
         options.setCapability("selenoid:options", new HashMap<String, Object>() {{
             put("name", "My test");
-            put("sessionTimeout", "1m");
-            put("screenResolution", "1920x1080x24");
-//            put("enableVNC", true);
+            put("sessionTimeout", "15m");
+            // put("screenResolution", "1920x1080x24");
+            // put("enableVNC", true);
             put("env", new ArrayList<String>() {{
                 add("TZ=UTC");
             }});
@@ -42,7 +39,7 @@ public class MainTest {
             put("enableVideo", true);
         }});
         driver = new RemoteWebDriver(new URL("http://194.67.119.85:4444/wd/hub"), options);
-        driver.manage().window().setSize(new Dimension(1920,1080));
+        // driver.manage().window().setSize(new Dimension(1920,1080));
         WebDriverRunner.setWebDriver(driver);
     }
 
@@ -53,11 +50,22 @@ public class MainTest {
 
     @Test
     public void simpleTest() {
-        open("https://google.com");
-        $(By.name("q")).setValue("text").pressEnter();
-//        $("#submit").click();
-//        $(".loading_progress").should(disappear); // Само подождёт, пока элемент исчезнет
-//        $("#username").shouldHave(text("Hello, Johny!")); // Само подождёт, пока у элемента появится нужный текст
-        assertEquals(1,1);
+        open("https://tatyana-aygi.ru/gallery");
+        Configuration.holdBrowserOpen = true;
+        $(By.id("user_login")).setValue("selenide");
+        $(By.id("user_pass")).setValue("junit5");
+        $(By.id("btn_login")).click();
+        $(By.id("wow-modal-close-1")).click();
+        $(By.id("al_2")).setSelected(true);
+        $(By.id("fl_3")).setSelected(true);
+        $(By.id("pr_4")).setSelected(true);
+        $(By.id("user_name")).scrollIntoView(true).setValue("test_name");
+        $(By.id("phone_number")).setValue("8-911-111-11-11");
+        $(By.id("e_mail")).setValue("mail@mail.com");
+        $(By.name("message")).setValue("Test message");
+        $(By.id("btn_id_main")).click();
+        $(By.id("btn_go")).click();
+        String txt = $(By.id("txt_result")).getText().substring(0, 37);
+        assertEquals(txt, "Ваш заказ на сумму 400 ₽ сформирован!");
     }
 }
