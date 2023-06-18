cc75428c63d665865504c2455465405c8ab29286
Krack Digger <grave1kro@gmail.com>
2023-06-18 18:39:54 +0300

    edit Jenkinsfile

--- src/test/java/ru/krackdigger/MainTest.java
+++ src/test/java/ru/krackdigger/MainTest.java
@@ -1,10 +1,7 @@
 package ru.krackdigger;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -14,13 +11,20 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static com.codeborne.selenide.Condition.image;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 
-    RemoteWebDriver driver;
+    private RemoteWebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        Configuration.baseUrl = "https://tatyana-aygi.ru";
+    }
 
     @BeforeEach
     public void initDriver() throws IOException {
@@ -40,7 +44,7 @@ public class MainTest {
         }});
         driver = new RemoteWebDriver(new URL("http://194.67.119.85:4444/wd/hub"), options);
         // driver.manage().window().setSize(new Dimension(1920,1080));
-        WebDriverRunner.setWebDriver(driver);
+//        WebDriverRunner.setWebDriver(driver);
     }
 
     @AfterEach
@@ -49,23 +53,73 @@ public class MainTest {
     }
 
     @Test
-    public void simpleTest() {
-        open("https://tatyana-aygi.ru/gallery");
+    @Tag("main_page")
+    public void mainPage() {
+        step("Открыть главную страницу", () -> {
+            open("/");
+        });
+        step("Проверить заголовок страницы", () -> {
+            String title = $(By.className("page-title")).getText();
+            assertEquals(title, "Фотограф Татьяна Айги");
+        });
+        step("Проверить наличие изображения в главной галерее", () -> {
+            $(By.className("swiper-slide-image")).shouldHave(image);
+        });
+        step("Проверить наличие подгалереи", () -> {
+            assert ($(By.className("foogallery")).isDisplayed());
+        });
+    }
+
+    @Test
+    @Tag("children_page")
+    public void childrenPage() {
+        step("Открыть галерею \"Дети\"", () -> {
+            open("/children");
+        });
+        step("Проверить заголовок страницы", () -> {
+            String title = $(By.className("page-title")).getText();
+            assertEquals(title, "Дети");
+        });
+        step("Проверить наличие галереи", () -> {
+            assert ($(By.className("elementor-image-gallery")).isDisplayed());
+        });
+    }
+
+    @Test
+    @Tag("create_order")
+    public void createOrder() {
         Configuration.holdBrowserOpen = true;
-        $(By.id("user_login")).setValue("selenide");
-        $(By.id("user_pass")).setValue("junit5");
-        $(By.id("btn_login")).click();
-        $(By.id("wow-modal-close-1")).click();
-        $(By.id("al_2")).setSelected(true);
-        $(By.id("fl_3")).setSelected(true);
-        $(By.id("pr_4")).setSelected(true);
-        $(By.id("user_name")).scrollIntoView(true).setValue("test_name");
-        $(By.id("phone_number")).setValue("8-911-111-11-11");
-        $(By.id("e_mail")).setValue("mail@mail.com");
-        $(By.name("message")).setValue("Test message");
-        $(By.id("btn_id_main")).click();
-        $(By.id("btn_go")).click();
-        String txt = $(By.id("txt_result")).getText().substring(0, 37);
-        assertEquals(txt, "Ваш заказ на сумму 400 ₽ сформирован!");
+        step("Открыть галерею", () -> {
+            open("/gallery");
+        });
+        step("Ввести логин и пароль", () -> {
+            $(By.id("user_login")).setValue("selenide");
+            $(By.id("user_pass")).setValue("junit5");
+            $(By.id("btn_login")).click();
+        });
+        step("Закрыть модальное окно", () -> {
+            $(By.id("wow-modal-close-1")).click();
+        });
+        step("Выбрать фотографии", () -> {
+            $(By.id("al_2")).setSelected(true);
+            $(By.id("fl_3")).setSelected(true);
+            $(By.id("pr_4")).setSelected(true);
+        });
+        step("Заполнить форму", () -> {
+            $(By.id("user_name")).scrollIntoView(true).setValue("test_name");
+            $(By.id("phone_number")).setValue("8-911-111-11-11");
+            $(By.id("e_mail")).setValue("mail@mail.com");
+            $(By.name("message")).setValue("Test message");
+        });
+        step("Перейти на экран подтверждения выбора", () -> {
+            $(By.id("btn_id_main")).click();
+        });
+        step("Подтвердить выбор", () -> {
+            $(By.id("btn_go")).click();
+        });
+        step("Проверка создания заказа", () -> {
+            String txt = $(By.id("txt_result")).getText().substring(0, 37);
+            assertEquals(txt, "Ваш заказ на сумму 400 ₽ сформирован!");
+        });
     }
 }
